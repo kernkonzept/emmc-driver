ad2896ac3d527aa6e662bcf8911ea90919fecfea
Frank Mehnert <frank.mehnert@kernkonzept.com>
2025-01-06 00:00:00 +0000

    mmio: throw error if reading from / writing to MMIO space fails
    Instead of printing a message, show a warning and throw an error.
    
    Change-Id: I02b2a8573a7bcd3f9b9ce29e9ab4d4abca09e5fe

--- server/src/mmio.cc
+++ server/src/mmio.cc
@@ -5,7 +5,7 @@
  * License: see LICENSE.spdx (in this directory or the directories above)
  */
 
-#include <cstdio>
+#include "debug.h"
 #include "mmio.h"
 
 namespace Hw {
@@ -14,19 +14,26 @@ l4_uint64_t
 Mmio_space_register_block_base::do_read(l4_addr_t addr, char log2_size) const
 {
   l4_uint64_t v;
-  if (_mmio_space->mmio_read(addr, log2_size, &v) == L4_EOK)
-    return v;
-  printf("do_read: %08lx not handled\n", addr);
-  return 0;
+  if (L4_UNLIKELY(_mmio_space->mmio_read(addr, log2_size, &v) < 0))
+    {
+      Dbg(Dbg::Warn, "mmio").printf("Could not read from offset %08lx", addr);
+      L4Re::throw_error(-L4_EIO, "Read register from MMIO space");
+    }
+  return v;
 }
 
 void
 Mmio_space_register_block_base::do_write(l4_uint64_t v,
                                          l4_addr_t addr, char log2_size) const
 {
-  if (_mmio_space->mmio_write(addr, log2_size, v) == L4_EOK)
-    return;
-  printf("do_write: %08lx not handled\n", addr);
+  if (L4_UNLIKELY(_mmio_space->mmio_write(addr, log2_size, v) < 0))
+    {
+      Dbg(Dbg::Warn, "mmio").printf("Could not write %08llx to offset %08lx",
+                                    v, addr);
+      L4Re::throw_error(-L4_EIO, "Write register to MMIO space");
+    }
+
+  return;
 }
 
 } // namespace Hw
