3b0fc67a7775bbdeb9a600bac1dd799e2c325729
Diego Dias <diego.dias@kernkonzept.com>
2026-01-12 00:00:00 +0000

    update copyright years
    Change-Id: I8284b298d1ce9e4057b6923903f54d66a9ecde39
    Signed-off-by: Diego Dias <diego.dias@kernkonzept.com>

--- server/src/main.cc
+++ server/src/main.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020-2021, 2023-2024 Kernkonzept GmbH.
+ * Copyright (C) 2020-2021, 2023-2025 Kernkonzept GmbH.
  * Author(s): Frank Mehnert <frank.mehnert@kernkonzept.com>
  *            Sarah Hoffmann <sarah.hoffmann@kernkonzept.com>
  *            Jakub Jermar <jakub.jermar@kernkonzept.com>
